b810a98cb120ac2467c68cceaedf657efb2c077a
sedar007 <dev@adjysedar.fr>
2025-09-22 00:33:46 +0200

    rm builds files

--- exemple/build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/adjysedardesir/Documents/Dev/network_pokemon/tests
-# Build directory: /Users/adjysedardesir/Documents/Dev/network_pokemon/exemple/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("networkPokemon")
