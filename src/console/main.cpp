8317efb2467d903490cfa5cd56166d9f09d7a692
sedar007 <dev@adjysedar.fr>
2026-01-28 23:22:54 +0100

    refactor CMakeLists.txt and main.cpp for improved structure and logging

--- src/console/main.cpp
+++ src/console/main.cpp
@@ -1,6 +1,13 @@
+#include "networkPokemon.h"
+
 int main(int argc, char* argv[]) {
 
+    bool traceLog = false;
     std::cout<<"Console"<<std::endl;
+
+    //pokemon::Main main(traceLog);
+    pokemon::Test test;
+    std::cout<<test.getIp()<<std::endl;
 
     return 0;
 }
